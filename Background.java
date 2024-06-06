e06d0f031e812067672755846214a61f23e998da
zoobsbaqai <zohairbaqai@hotmail.com>
2024-06-06 10:38:20 -0400

    Adding background features for testing

--- Background.java
+++
@@ -1,6 +0,0 @@
-import javax.swing.*;
-public class Background{
-    
-    //https://www.naukri.com/code360/library/displaying-image-in-java-swing
-
-}
---
+++ ImageJFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+ 
+public class ImageJFrame
+{
+  ImageJFrame() 
+  {
+    JFrame f = new JFrame("Add an image to JFrame");
+    ImageIcon icon = new ImageIcon("oldmap2.jpg");
+    f.add(new JLabel(icon));
+    f.pack();
+    f.setVisible(true);
+  }
+  public static void main(String args[]) 
+  {
+    new ImageJFrame();
+  }
+}
